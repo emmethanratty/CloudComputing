04272fe03e43ce2607bed984b8ce6522e9378531
emmethanratty <c13502317@mydit.ie>
2015-09-30 14:34:07 +0000

    added for loop and count the string

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -1,6 +1,16 @@
 
 # Input function lets you input a string into the variable
 string = input("Please enter a string: ")
+count = 0
 
 #print prints to the console
 print(string)
+
+stringlen = len(string )
+
+print(stringlen)
+
+for i  in range(0, stringlen):
+  count = count + 1
+
+print(count)
